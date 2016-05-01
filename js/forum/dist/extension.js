54da5d6a42a698fed6c263b7653a6e20a58ebe12
Connor <connor0095@gmail.com>
2016-05-01 00:27:57 -0400

    Fix CSS and JS append typo

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -19,8 +19,8 @@ System.register('Davis/CustomHeader/main', ['flarum/app', 'flarum/extend', 'flar
                     $('#customHeader').append(app.forum.attribute('davis-customheader.customheader'));
                     var toppadding = Number(app.forum.attribute("davis-customheader.heightofheader")) + 52;
                     $('head').append('<style>@media (min-width: 768px){#header {height: ' + app.forum.attribute("davis-customheader.heightofheader") + 'px !important;padding: 0 !important;}#flarumHeader {height: 52px; padding: 8px;}.App {padding-top: ' + toppadding + 'px !important;}.App:before {height: ' + toppadding + 'px;}#customHeader{height:' + app.forum.attribute("davis-customheader.heightofheader") + 'px;}}</style>');
-                    $('head').append('<style>' + app.forum.attribute('davis-customheader.cssofheader' + '</style>'));
-                    $('head').append('<script>' + app.forum.attribute('davis-customheader.jsofheader' + '</script>'));
+                    $('head').append('<style>' + app.forum.attribute("davis-customheader.cssofheader") + '</style>');
+                    $('head').append('<script>' + app.forum.attribute("davis-customheader.jsofheader") + '</script>');
                 });
             });
         }
--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -9,7 +9,7 @@ app.initializers.add('davis-customheader-forum', function() {
         $('#customHeader').append(app.forum.attribute('davis-customheader.customheader'));
         const toppadding = Number(app.forum.attribute("davis-customheader.heightofheader")) + 52;
         $('head').append('<style>@media (min-width: 768px){#header {height: '+app.forum.attribute("davis-customheader.heightofheader")+'px !important;padding: 0 !important;}#flarumHeader {height: 52px; padding: 8px;}.App {padding-top: '+toppadding+'px !important;}.App:before {height: '+toppadding+'px;}#customHeader{height:'+app.forum.attribute("davis-customheader.heightofheader")+'px;}}</style>');
-        $('head').append('<style>'+app.forum.attribute('davis-customheader.cssofheader'+'</style>'));
-        $('head').append('<script>'+app.forum.attribute('davis-customheader.jsofheader'+'</script>'));
+        $('head').append('<style>'+app.forum.attribute("davis-customheader.cssofheader")+'</style>');
+        $('head').append('<script>'+app.forum.attribute("davis-customheader.jsofheader")+'</script>');
     });
 });
